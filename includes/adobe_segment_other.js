c422930e4e27294d994501a19928dc7b0ea69161
Claudia Carrubba <google-oauth2|106910435596431382258>
2023-02-21 08:44:18 +0000

    --

--- includes/adobe_segment_other.js
+++ includes/adobe_segment_other.js
@@ -6,10 +6,11 @@
 module.exports = () => {
     return `
         case 
-            when lower(segment_name) like '%other%' then 'Other'
+            when lower(segment_name) like '%direct%' then 'Direct'
+            when lower(segment_name) like '%organic%' then 'Organic Search'
+            when lower(segment_name) like '%referral%' then 'Referral'
         else 
-            'N/A' 
+            'Other' 
         end
     `
 } 
-
